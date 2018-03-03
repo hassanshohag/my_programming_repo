56d973a20bc9eb286710f8f3f82929f02bd19a08
hassanshohag <hassan_shohag@yahoo.com>
2018-03-03 13:34:01 +0100

    More files are added

---
+++ java/practice/practice_1.java
@@ -0,0 +1,10 @@
+class practice_1
+{
+  public static void main(String[] args) {
+    float a=4.3f, b=0.0f;
+    float c;
+    c=a%b;
+    System.out.println(c);
+  }
+
+}
---
+++ java/practice/practive.java
@@ -0,0 +1,9 @@
+class practice1
+{
+  public static void main(String[] args)
+  {
+    byte b1=127, b2=1;
+    byte sum = (byte)(b1 / b2);
+    System.out.println(MIN_VALUE);
+    }
+}
